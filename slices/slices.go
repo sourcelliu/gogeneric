4e4421b611200d3bfecef1fad0947d032e50f38b
sourcelliu <sourcelliu@tencent.com>
2022-05-02 17:23:36 +0800

    adjust map init size.

--- slices/slices.go
+++ slices/slices.go
@@ -6,7 +6,7 @@ func SliceToMap[T any, E constraints.Ordered](v []T, f func(T) E) (m map[E]T) {
 	if len(v) == 0 || f == nil {
 		return nil
 	}
-	m = make(map[E]T, 0)
+	m = make(map[E]T, len(v))
 	for _, val := range v {
 		m[f(val)] = val
 	}
